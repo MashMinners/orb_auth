dfd5efaef53d31472cd94ea5df90de051aec74fc
Rain <Valehtelit@mail.ru>
2023-07-05 22:10:46 +1000

    #Настройка архитектуры

--- src/Authorization/AuthorizationMiddleware.php
+++ src/Authorization/AuthorizationMiddleware.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace ORB\Authorization;
 
-class AuthorizationMiddleware
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class AuthorizationMiddleware implements MiddlewareInterface
 {
 
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /*
+         * Получает запрос
+         * Читает заголовок Authorization: Bearer ey......
+         * Расшифровывает токен
+         */
+    }
 }
